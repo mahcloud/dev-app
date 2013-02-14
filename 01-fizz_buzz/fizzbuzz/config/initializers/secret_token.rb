89ac23f3e0858e67d56aa0575dc441d413e7c317
Michael Hoffert <mike.hoffert@gmail.com>
2013-02-14 09:21:17 -0600

    Rails Framework

---
+++ 01-fizz_buzz/fizzbuzz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fizzbuzz::Application.config.secret_token = '4f373cfed186111f747f3ead218ec2e9cbe568b6394a0d001d681b1bf61c24ed758183faf3d9167706d18fc873c9255f3de19f029c6a864ac358f3e7f14f5078'
