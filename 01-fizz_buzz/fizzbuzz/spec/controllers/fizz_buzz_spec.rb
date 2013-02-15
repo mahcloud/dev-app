8cb2043729eaa602ea6a6f79b2c89c1608222be8
Michael Hoffert <mike.hoffert@gmail.com>
2013-02-15 07:49:16 -0600

    Fizzbuzz controller spec shell until I figure out how to use rspec for routes.

---
+++ 01-fizz_buzz/fizzbuzz/spec/controllers/fizz_buzz_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe FizzbuzzController do
+  describe "#number" do
+    it "is number" do
+    end
+  end
+
+  describe "#range" do
+    it "is range" do
+    end
+  end
+end
